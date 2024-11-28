84a8bdf70cab0b9355442d9d5de0fc52ec6c69e0
bineraluno <abnerviniciusf@gmail.com>
2024-11-28 11:40:03 -0300

    4124141421

---
+++ escola/src/Paginas/Alteracao.jsx
@@ -0,0 +1,7 @@
+export default function Alteracao(){
+    return(
+        <div>
+            <h1>Alteração de alunos</h1>
+        </div>
+    );
+}
---
+++ escola/src/Paginas/Exclusao.jsx
@@ -0,0 +1,7 @@
+export default function Exclusão(){
+    return(
+        <div>
+            <h1>Exclusão de alunos</h1>
+        </div>
+    );
+}
